24fe41aef919133c2537f797e1b8346b377680d3
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-10-21 15:54:37 +0200

    Add base URL constant

---
+++ src/ApodDotnet/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace ApodDotnet
+{
+    internal static class Constants
+    {
+        /// <summary>The base URL to make the HTTP GET request to.</summary>
+        internal const string BaseUrl = "https://api.nasa.gov/planetary/apod";
+    }
+}
