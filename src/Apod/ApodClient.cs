79288cadb8b70bef96672c0b3943541ea25c348c
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-10-22 00:33:06 +0200

    Add support for multiple apod results

--- src/Apod/ApodClient.cs
+++ src/Apod/ApodClient.cs
@@ -27,18 +27,45 @@ namespace Apod
             _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         }
 
+        /// <summary>Fetch the current Astronomy Picture of the Day.</summary>
         public async Task<AstronomyContent> FetchApodAsync()
         {
             var responseMessage = await FetchApiDataAsync();
-            return await ResponseToAstronomyContentAsync(responseMessage);
+            return await GetOneApodResult(responseMessage);
         }
 
+        /// <summary>Fetch the Astronomy Picture of the Day for a specific date.</summary>
+        /// <param name="date">The date to request the APOD for. Must be between 1995-06-20 and today's date.</param>
         public async Task<AstronomyContent> FetchApodAsync(DateTime date)
         {
+            // TODO: Error handling for invalid dates
+
             var queryParameter = $"date={date.ToString("yyyy-MM-dd")}";
             var responseMessage = await FetchApiDataAsync(queryParameter);
-            return await ResponseToAstronomyContentAsync(responseMessage);
+            return await GetOneApodResult(responseMessage);
         }
+
+        /// <summary>Fetch all the Astronomy Pictures of the Day between two dates.</summary>
+        /// <param name="startDate">The start date. Must be between 1995-06-16 and today's date.</param>
+        /// <param name="endDate">The end date. Must be between the <paramref name="startDate"/> and today's date. Defaults to <see cref="DateTime.Today"/>.</param>
+        public async Task<AstronomyContent[]> FetchApodAsync(DateTime startDate, DateTime endDate = default)
+        {
+            if (!DateIsInRange(startDate))                     { throw new DateOutOfRangeException(nameof(startDate), startDate); }
+            if (endDate != default && !DateIsInRange(endDate)) { throw new DateOutOfRangeException(nameof(endDate), endDate); }
+            if (DateTime.Compare(startDate, endDate) > 0)      { throw new DateOutOfRangeException("The start date can not be after the end date."); }
+
+            var startDateString = $"start_date={startDate.ToString("yyyy-MM-dd")}";
+
+            var endDateString = endDate == default
+                ? string.Empty 
+                : $"end_date={endDate.ToString("yyyy-MM-dd")}";
+
+            var responseMessage = await FetchApiDataAsync(startDateString, endDateString);
+            return await GetMultipleApodResults(responseMessage);
+        }
+
+        private bool DateIsInRange(DateTime dateTime)
+            => (DateTime.Compare(dateTime, DateTime.Today) < 0) && (DateTime.Compare(dateTime, Constants.FirstApodDate) > 0);
 
         private async Task<HttpResponseMessage> FetchApiDataAsync(params string[] queryParameters)
         {
@@ -47,7 +74,7 @@ namespace Apod
             return await _httpClient.SendAsync(requestMessage);
         }
 
-        private async Task<AstronomyContent> ResponseToAstronomyContentAsync(HttpResponseMessage responseMessage)
+        private async Task<AstronomyContent> GetOneApodResult(HttpResponseMessage responseMessage)
         {
             var responseContent = await responseMessage.Content.ReadAsStringAsync();
 
@@ -60,7 +87,20 @@ namespace Apod
             return JsonSerializer.Deserialize<AstronomyContent>(responseContent, _jsonSerializerOptions);
         }
 
-        private string BuildFullQueryString(params string[] queryParameters)
+        private async Task<AstronomyContent[]> GetMultipleApodResults(HttpResponseMessage responseMessage)
+        {
+            var responseContent = await responseMessage.Content.ReadAsStringAsync();
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                var errorResponse = JsonSerializer.Deserialize<ErrorResponse>(responseContent, _jsonSerializerOptions);
+                errorResponse.ThrowInformativeError();
+            }
+
+            return JsonSerializer.Deserialize<AstronomyContent[]>(responseContent, _jsonSerializerOptions);
+        }
+
+        private string BuildFullQueryString(string[] queryParameters)
         {
             var stringBuilder = new StringBuilder();
 
@@ -70,6 +110,7 @@ namespace Apod
 
             foreach (var parameter in queryParameters)
             {
+                if (string.IsNullOrWhiteSpace(parameter)) { continue; }
                 stringBuilder.Append("&");
                 stringBuilder.Append(parameter);
             }
--- src/Apod/Constants.cs
+++ src/Apod/Constants.cs
@@ -1,8 +1,13 @@
-﻿namespace Apod
+﻿using System;
+
+namespace Apod
 {
     internal static class Constants
     {
         /// <summary>The base URL to make the HTTP GET request to.</summary>
         internal const string BaseUrl = "https://api.nasa.gov/planetary/apod";
+
+        /// <summary>The publication date of the very first Astronomy Picture of the Day.</summary>
+        internal static readonly DateTime FirstApodDate = DateTime.Parse("1995-06-16");
     }
 }
