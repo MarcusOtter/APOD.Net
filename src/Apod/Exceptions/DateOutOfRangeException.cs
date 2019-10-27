db3db02dd22bb9e43f7a3f835511fa64358d62e9
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-10-27 15:15:46 +0100

    Fix typo

--- src/Apod/Exceptions/DateOutOfRangeException.cs
+++ src/Apod/Exceptions/DateOutOfRangeException.cs
@@ -9,6 +9,6 @@ namespace Apod
         public DateOutOfRangeException(string message, Exception inner) : base(message, inner) { }
         public DateOutOfRangeException(string parameterName, string message) : base(parameterName, message) { }
         public DateOutOfRangeException(string parameterName, DateTime dateTime) 
-            : base(parameterName, $"The parameter \"{parameterName}\" must to be between 1995-06-16 and today's date. (Date provided: {dateTime.ToString("yyyy-MM-dd")})") { }
+            : base(parameterName, $"The parameter \"{parameterName}\" must be between 1995-06-16 and today's date. (Date provided: {dateTime.ToString("yyyy-MM-dd")})") { }
     }
 }
