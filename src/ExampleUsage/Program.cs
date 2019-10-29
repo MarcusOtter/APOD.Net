16eae6041ed22e91e67f161fcc535fcc06fb58b6
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-10-29 12:29:23 +0100

    Add example project

---
+++ src/ExampleUsage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Apod;
+
+namespace ApodExample
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            var apodClient = new ApodClient("DEMO_KEY");
+            var apod = await apodClient.FetchApodAsync();
+
+            Console.WriteLine(apod.Title);
+            Console.WriteLine(apod.Explanation);
+            Console.WriteLine(apod.ContentUrlHD);
+        }
+    }
+}
