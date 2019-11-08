d1665e05fce87f1aca0c35e8576f48c581cdf9a5
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-11-08 15:32:12 +0100

    Add error builder tests

---
+++ src/ApodTests/ErrorBuilderTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xunit;
+using Apod.Logic.Errors;
+using Apod;
+
+namespace ApodTests
+{
+    public class ErrorBuilderTests
+    {
+        [Theory]
+        [InlineData("MMMM dd, yyyy", "April 21, 1992", "January 02, 2049")]
+        [InlineData("dd/MM-yyyy", "21/04-1992", "02/01-2049")]
+        [InlineData("ddMMyyyy", "21041992", "02012049")]
+        public void GetDateOutOfRangeError_CorrectCustomDateFormat(string dateFormat, string expectedFirstValidDate, string expectedLastValidDate)
+        {
+            var firstValidDate = new DateTime(1992, 04, 21);
+            var lastValidDate = new DateTime(2049, 01, 02);
+            var errorBuilder = new ErrorBuilder(dateFormat);
+
+            var actual = errorBuilder.GetDateOutOfRangeError(firstValidDate, lastValidDate);
+            
+            Assert.Contains(expectedFirstValidDate, actual.ErrorMessage);
+            Assert.Contains(expectedLastValidDate, actual.ErrorMessage);
+        }
+
+        [Fact]
+        public void GetDateOutOfRangeError_CorrectDefaultDateFormat()
+        {
+            var firstValidDate = new DateTime(2002, 02, 22);
+            var lastValidDate = new DateTime(2073, 10, 05);
+            var errorBuilder = new ErrorBuilder();
+
+            var expectedFirstValidDate = "February 22 2002";
+            var expectedLastValidDate = "October 05 2073";
+
+            var actual = errorBuilder.GetDateOutOfRangeError(firstValidDate, lastValidDate);
+
+            Assert.Contains(expectedFirstValidDate, actual.ErrorMessage);
+            Assert.Contains(expectedLastValidDate, actual.ErrorMessage);
+        }
+
+        [Fact]
+        public void GetDateOutOfRangeError_CorrectErrorCode()
+        {
+            var firstValidDate = new DateTime(1997, 03, 12);
+            var lastValidDate = new DateTime(2005, 07, 01);
+            var errorBuilder = new ErrorBuilder();
+
+            var expected = ApodErrorCode.BadRequest;
+
+            var actual = errorBuilder.GetDateOutOfRangeError(firstValidDate, lastValidDate);
+
+            Assert.Equal(expected, actual.ErrorCode);
+        }
+
+        [Fact]
+        public void GetStartDateAfterEndDateError_CorrectErrorCode()
+        {
+            var errorBuilder = new ErrorBuilder();
+
+            var expected = ApodErrorCode.BadRequest;
+
+            var actual = errorBuilder.GetStartDateAfterEndDateError();
+
+            Assert.Equal(expected, actual.ErrorCode);
+        }
+    }
+}
