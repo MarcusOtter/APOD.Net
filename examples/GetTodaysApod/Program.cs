88093ef6283a900f1279f4dd88f65b110171cf7e
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-11-21 20:08:08 +0100

    Move examples to new folder

---
+++ examples/GetTodaysApod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetTodaysApod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New examples will come when the NuGet package is released.");
+        }
+    }
+}
